48702b7a023bc85ea479e9009b9026fb97424dfd
MaxWhitehead1999 <157076920+MaxWhitehead1999@users.noreply.github.com>
2026-07-21 18:48:45 -0400

    July 21st Login page and main page plus nav start
    Created the html and css for the login page which includes the signup button which brings the user to the signup page, the login button which currently brings the user to the main page, and the forgot password hyperlink which brings the user to the forgot password page, of course none of the security is enabled right now this was just for testing the hyperlinks and buttons work, the actual security of the users will come later. I have also got a start on the navbar and main apge html, but not by much, im hoping to get both of them done during my next day off.

---
+++ script.js
@@ -0,0 +1,19 @@
+//use this please: file:///C:/Users/Maxwe/OneDrive/Documents/Software%20Capstone%20Proposal/CapstoneProject/LibraryWebsite/login.html
+
+
+function goToSignup() {
+    window.location.href = "signup.html";
+}
+
+function goToMain() {
+    window.location.href = "main.html";
+}
+
+function forgotPassword() {
+    window.location.href = "login.html";
+}
+
+function logout() {
+    window.location.href = "login.html";
+}
+
