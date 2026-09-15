79da9647d9f9a8177e5f7030682ae92e7d220039
MaxWhitehead1999 <157076920+MaxWhitehead1999@users.noreply.github.com>
2026-09-15 15:51:26 -0400

    August Updates
    added Error page along with pop up message for the page once user initially enters, added new book entry page for librarian access, only basic html to start, added and created menu ribbon for the top of most pages, but removed it as a separate html for now, updated javascript so that the user can click to enter different pages as well as the ability to login and access the rest of the website and sends the user to the error page if incorrect (this is to be worked on in the future so things may change going forward), general html and css changes to make the website look more organized and professional.

--- script.js
+++ script.js
@@ -5,9 +5,31 @@ function goToSignup() {
     window.location.href = "signup.html";
 }
 
+/** Pass and username requirements */
+const storedEmail = "test@email.com";
+const storedPassword = "password123";
+const maxAttempts = 5;
+const lockoutDuration = 30 * 60 * 1000; 
+
+
+
+
+/**fix later so that after 5 attempts the error page is displayed then have it be on for 30 minutes */
 function goToMain() {
-    window.location.href = "main.html";
+
+    if (
+        document.getElementById("email").value === storedEmail &&
+        document.getElementById("password").value === storedPassword
+    ) {
+        window.location.href = "main.html";
+    } else {
+        window.location.href = "error.html";
+    }
 }
+
+
+
+
 
 function forgotPassword() {
     window.location.href = "login.html";
@@ -16,4 +38,9 @@ function forgotPassword() {
 function logout() {
     window.location.href = "login.html";
 }
+
+function closeErrorAlert(){
+    document.getElementById("errorAlert").style.display = "none";
+}
+
 
